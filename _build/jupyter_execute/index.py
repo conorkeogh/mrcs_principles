d7acb10c7f9a79dde9a62d5fb3003ca1c266b35d
Conor Keogh <conor.keogh@nds.ox.ac.uk>
2021-10-03 14:03:16 +0100

    Updated configuration
    Added github buttons

--- _build/jupyter_execute/index.py
+++ _build/jupyter_execute/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Principle of neurosurgical science
+# # Principles of neurosurgical science
 # Revision notes for the MRCS examination
 
 # ## Introduction
